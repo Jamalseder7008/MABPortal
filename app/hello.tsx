890ee66e4a1f188abe2e3bbcc047c8d8c5dfc03e
jmseder <jmseder@uno.edu>
2024-05-27 10:35:29 -0400

    new features

---
+++ app/hello.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <Text style={{ color: '#fff' }}>Hello world!</Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#25292e',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    imageContainer: {
+      flex: 1,
+      paddingTop: 58,
+    },
+    image: {
+      width: 320,
+      height: 440,
+      borderRadius: 18,
+    },
+  });
